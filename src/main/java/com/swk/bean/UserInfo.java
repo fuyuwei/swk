f31a457cb7af39cc6e1c864b38c5b24b85b4e22c
fuyuwei <fuyuwei_cool@sina.com>
2017-07-08 22:23:17 +0800

    init ssm

---
+++ src/main/java/com/swk/bean/UserInfo.java
@@ -0,0 +1,413 @@
+package com.swk.bean;
+
+public class UserInfo {
+    private Long id;
+
+    private Long loginId;
+
+    private Long time;
+
+    private String realityName;
+
+    private String mobile;
+
+    private String idNumber;
+
+    private Integer sex;
+
+    private Integer educationId;
+
+    private Integer maritalId;
+
+    private Integer houseId;
+
+    private String recommendUserId;
+
+    private Long balance;
+
+    private Long freeze;
+
+    private String ipsAcctNo;
+
+    private Integer usertype;
+
+    private String hStreet;
+
+    private Integer hPostcode;
+
+    private String jStreet;
+
+    private Integer jPostcode;
+
+    private Integer dIdType;
+
+    private Integer dChildType;
+
+    private Integer dHProvinceId;
+
+    private Integer dHCityId;
+
+    private Integer dHCountryId;
+
+    private Integer dJProvinceId;
+
+    private Integer dJCityId;
+
+    private Integer dJCountryId;
+
+    private String bankAccountId;
+
+    private String dBankCode;
+
+    private String dBankName;
+
+    private String userBankName;
+
+    private Long maxCredit;
+
+    private Integer dBankCityId;
+
+    private Byte lockLoseMoney;
+
+    private Long monthlyPay;
+
+    private String dBankCode2;
+
+    private String bankAccountId2;
+
+    private Byte emailStatus;
+
+    private String email;
+
+    private Integer userkind;
+
+    private Integer platform;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(Long loginId) {
+        this.loginId = loginId;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    public String getRealityName() {
+        return realityName;
+    }
+
+    public void setRealityName(String realityName) {
+        this.realityName = realityName == null ? null : realityName.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber == null ? null : idNumber.trim();
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getEducationId() {
+        return educationId;
+    }
+
+    public void setEducationId(Integer educationId) {
+        this.educationId = educationId;
+    }
+
+    public Integer getMaritalId() {
+        return maritalId;
+    }
+
+    public void setMaritalId(Integer maritalId) {
+        this.maritalId = maritalId;
+    }
+
+    public Integer getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(Integer houseId) {
+        this.houseId = houseId;
+    }
+
+    public String getRecommendUserId() {
+        return recommendUserId;
+    }
+
+    public void setRecommendUserId(String recommendUserId) {
+        this.recommendUserId = recommendUserId == null ? null : recommendUserId.trim();
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
+
+    public Long getFreeze() {
+        return freeze;
+    }
+
+    public void setFreeze(Long freeze) {
+        this.freeze = freeze;
+    }
+
+    public String getIpsAcctNo() {
+        return ipsAcctNo;
+    }
+
+    public void setIpsAcctNo(String ipsAcctNo) {
+        this.ipsAcctNo = ipsAcctNo == null ? null : ipsAcctNo.trim();
+    }
+
+    public Integer getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(Integer usertype) {
+        this.usertype = usertype;
+    }
+
+    public String gethStreet() {
+        return hStreet;
+    }
+
+    public void sethStreet(String hStreet) {
+        this.hStreet = hStreet == null ? null : hStreet.trim();
+    }
+
+    public Integer gethPostcode() {
+        return hPostcode;
+    }
+
+    public void sethPostcode(Integer hPostcode) {
+        this.hPostcode = hPostcode;
+    }
+
+    public String getjStreet() {
+        return jStreet;
+    }
+
+    public void setjStreet(String jStreet) {
+        this.jStreet = jStreet == null ? null : jStreet.trim();
+    }
+
+    public Integer getjPostcode() {
+        return jPostcode;
+    }
+
+    public void setjPostcode(Integer jPostcode) {
+        this.jPostcode = jPostcode;
+    }
+
+    public Integer getdIdType() {
+        return dIdType;
+    }
+
+    public void setdIdType(Integer dIdType) {
+        this.dIdType = dIdType;
+    }
+
+    public Integer getdChildType() {
+        return dChildType;
+    }
+
+    public void setdChildType(Integer dChildType) {
+        this.dChildType = dChildType;
+    }
+
+    public Integer getdHProvinceId() {
+        return dHProvinceId;
+    }
+
+    public void setdHProvinceId(Integer dHProvinceId) {
+        this.dHProvinceId = dHProvinceId;
+    }
+
+    public Integer getdHCityId() {
+        return dHCityId;
+    }
+
+    public void setdHCityId(Integer dHCityId) {
+        this.dHCityId = dHCityId;
+    }
+
+    public Integer getdHCountryId() {
+        return dHCountryId;
+    }
+
+    public void setdHCountryId(Integer dHCountryId) {
+        this.dHCountryId = dHCountryId;
+    }
+
+    public Integer getdJProvinceId() {
+        return dJProvinceId;
+    }
+
+    public void setdJProvinceId(Integer dJProvinceId) {
+        this.dJProvinceId = dJProvinceId;
+    }
+
+    public Integer getdJCityId() {
+        return dJCityId;
+    }
+
+    public void setdJCityId(Integer dJCityId) {
+        this.dJCityId = dJCityId;
+    }
+
+    public Integer getdJCountryId() {
+        return dJCountryId;
+    }
+
+    public void setdJCountryId(Integer dJCountryId) {
+        this.dJCountryId = dJCountryId;
+    }
+
+    public String getBankAccountId() {
+        return bankAccountId;
+    }
+
+    public void setBankAccountId(String bankAccountId) {
+        this.bankAccountId = bankAccountId == null ? null : bankAccountId.trim();
+    }
+
+    public String getdBankCode() {
+        return dBankCode;
+    }
+
+    public void setdBankCode(String dBankCode) {
+        this.dBankCode = dBankCode == null ? null : dBankCode.trim();
+    }
+
+    public String getdBankName() {
+        return dBankName;
+    }
+
+    public void setdBankName(String dBankName) {
+        this.dBankName = dBankName == null ? null : dBankName.trim();
+    }
+
+    public String getUserBankName() {
+        return userBankName;
+    }
+
+    public void setUserBankName(String userBankName) {
+        this.userBankName = userBankName == null ? null : userBankName.trim();
+    }
+
+    public Long getMaxCredit() {
+        return maxCredit;
+    }
+
+    public void setMaxCredit(Long maxCredit) {
+        this.maxCredit = maxCredit;
+    }
+
+    public Integer getdBankCityId() {
+        return dBankCityId;
+    }
+
+    public void setdBankCityId(Integer dBankCityId) {
+        this.dBankCityId = dBankCityId;
+    }
+
+    public Byte getLockLoseMoney() {
+        return lockLoseMoney;
+    }
+
+    public void setLockLoseMoney(Byte lockLoseMoney) {
+        this.lockLoseMoney = lockLoseMoney;
+    }
+
+    public Long getMonthlyPay() {
+        return monthlyPay;
+    }
+
+    public void setMonthlyPay(Long monthlyPay) {
+        this.monthlyPay = monthlyPay;
+    }
+
+    public String getdBankCode2() {
+        return dBankCode2;
+    }
+
+    public void setdBankCode2(String dBankCode2) {
+        this.dBankCode2 = dBankCode2 == null ? null : dBankCode2.trim();
+    }
+
+    public String getBankAccountId2() {
+        return bankAccountId2;
+    }
+
+    public void setBankAccountId2(String bankAccountId2) {
+        this.bankAccountId2 = bankAccountId2 == null ? null : bankAccountId2.trim();
+    }
+
+    public Byte getEmailStatus() {
+        return emailStatus;
+    }
+
+    public void setEmailStatus(Byte emailStatus) {
+        this.emailStatus = emailStatus;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public Integer getUserkind() {
+        return userkind;
+    }
+
+    public void setUserkind(Integer userkind) {
+        this.userkind = userkind;
+    }
+
+    public Integer getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(Integer platform) {
+        this.platform = platform;
+    }
+}
---
+++ src/main/java/com/swk/service/IUserInfoService.java
@@ -0,0 +1,8 @@
+package com.swk.service;
+
+import com.swk.bean.UserInfo;
+
+public interface IUserInfoService {
+
+	public UserInfo getUserInfoById(long id); 
+}
